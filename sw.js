db84141ad66c558d61201f09daeb0352e405a55f
JaeJet <98198415+JaeJet@users.noreply.github.com>
2024-11-23 13:38:40 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,30 +8,32 @@ self.addEventListener('install', event => {
       '/',
       //'/converter.js',
       '/styles.css',
-      '/Compositions.html'
+      '/Compositions.html',
+      '/index.html',
+      '/about.html',
     ]);
   })());
 });
 
 self.addEventListener('fetch', event => {
-   event.respondWith((async () => {
-     const cache = await caches.open(CACHE_NAME);
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
 
-     // Get the resource from the cache.
-     const cachedResponse = await cache.match(event.request);
-     if (cachedResponse) {
-       return cachedResponse;
-     } else {
-         try {
-           // If the resource was not in the cache, try the network.
-           const fetchResponse = await fetch(event.request);
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
 
-           // Save the resource in the cache and return it.
-           cache.put(event.request, fetchResponse.clone());
-           return fetchResponse;
-         } catch (e) {
-           // The network failed.
-         }
-     }
-   })());
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
 });
